7613f71f1c5a28b6b61152fcec7119bbd2adfec9
wiseman947 <wisdom.go.wo@gmail.com>
2026-04-18 16:03:04 +0100

    fix the error

--- Frontend/app/admin/components/DeliveryMap.tsx
+++ Frontend/app/admin/components/DeliveryMap.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// import { MapContainer, TileLayer, Marker } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
 
 export default function DeliveryMap() {
   return (
