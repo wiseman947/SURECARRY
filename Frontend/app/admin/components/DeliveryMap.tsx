dbd217df058269bfeffcf68df67d42d5f709859d
wiseman947 <wisdom.go.wo@gmail.com>
2026-04-18 15:49:36 +0100

    fix a bug

--- Frontend/app/admin/components/DeliveryMap.tsx
+++ Frontend/app/admin/components/DeliveryMap.tsx
@@ -9,17 +9,6 @@ export default function DeliveryMap() {
 
       <h2 className="font-semibold mb-4">Live Deliveries</h2>
 
-      <MapContainer
-        center={[6.5244, 3.3792] as any}
-        zoom={13}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <Marker position={[6.5244, 3.3792]} />
-      </MapContainer>
 
     </div>
   );
